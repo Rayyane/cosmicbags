b3ce8529f469f13a6c32a7e2213f3a32db259283
Rayyane <akibmr9@gmail.com>
2023-05-15 21:00:10 +0600

    showcase

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+AOS.init();
+
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 3,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+var swiper3 = new Swiper(".mySwiper3", {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  /*pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },*/
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+  },
+});
